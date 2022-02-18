d8c3190cb46085ec5726ce34779f68cf09df3bc1
Gabriel Linhares <glinharesb@gmail.com>
2022-02-18 16:50:15 -0300

    feat: added Produto migration and entity

---
+++ src/database/migrations/1645213378368-CreateProduto.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProduto1645213378368 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'produtos',
+        columns: [
+          {
+            name: 'codigo_produto',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'nome',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'fabricao',
+            type: 'varchar',
+          },
+          {
+            name: 'tamanho',
+            type: 'varchar',
+          },
+          {
+            name: 'valor',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('produtos');
+  }
+}
