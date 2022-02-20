d0995dbd59d3845c5fbd23fdf06b010280efb063
Gabriel Linhares <glinharesb@gmail.com>
2022-02-20 19:38:58 -0300

    fix: added connectToDatabase and error handling

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,33 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+export async function connectToDatabase() {
+  try {
+    // connect using .env variables
+    await createConnection();
+
+    console.log('~> The database started successfully');
+    return true;
+  } catch (error) {
+    console.log(`~> Error: ${error.message}`);
+
+    try {
+      console.log('~> Trying to connect with sqlite...');
+
+      // connect using sqlite
+      await createConnection({
+        type: 'sqlite',
+        database: './src/database/db.sqlite3',
+        migrationsRun: true,
+        migrations: ['./src/database/migrations/*.ts'],
+        entities: ['./src/entities/*.ts'],
+      });
+
+      console.log('~> Successfully :)');
+      return true;
+    } catch (error) {
+      console.log(`~> Failed again, error: ${error.message}`);
+    }
+  }
+
+  return false;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import 'reflect-metadata';
 import express from 'express';
 
-import './database';
+import { connectToDatabase } from './database';
 import { routes } from './routes';
 
-const app = express();
+async function bootstrap() {
+  const connection = await connectToDatabase();
+  if (!connection) return;
 
-app.use(express.json());
+  const app = express();
+  app.use(express.json());
+  app.use(routes);
 
-app.use(routes);
+  const PORT = Number(process.env.APP_PORT) || 3000;
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`~> Server listening on: http://localhost:${PORT}`);
+  });
+}
 
-const PORT = Number(process.env.APP_PORT) || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server listening on: http://localhost:${PORT}`);
-});
+bootstrap();
